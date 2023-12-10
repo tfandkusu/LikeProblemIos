6d08ace3a163b17edf745e3ec5a75de6f0bcff18
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-12-10 15:43:17 +0900

    Initial Commit

---
+++ LikeProblemIos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LikeProblemIos
+//
+//  Created by toya on 2023/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LikeProblemIos/LikeProblemIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeProblemIosApp.swift
+//  LikeProblemIos
+//
+//  Created by toya on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct LikeProblemIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
