3de3fd1f5b076d4f8d3c68be6c7cf373d01f3cdf
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-12-13 00:28:48 +0900

    Swiftformat を導入

--- LikeProblemIosTests/LikeProblemIosTests.swift
+++ LikeProblemIosTests/LikeProblemIosTests.swift
@@ -5,11 +5,10 @@
 //  Created by toya on 2023/12/10.
 //
 
-import XCTest
 @testable import LikeProblemIos
+import XCTest
 
 final class LikeProblemIosTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class LikeProblemIosTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
